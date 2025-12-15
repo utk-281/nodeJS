586314f245e83eb0685b57eb2459fb7cf6762f5a
utkarsh <utkarshgupta.281@gmail.com>
2025-12-15 13:26:07 +0530

    add fs async intro

--- Node/Modules/Built In/fs.js
+++ Node/Modules/Built In/fs.js
@@ -78,8 +78,12 @@ const fs = require("node:fs"); // name of the module
 // }
 
 //? 5) creating a folder
-// method name --> mkdirSync()
-// syntax --> mkdirSync("path/name")
+// method name --> mkdirSync() --> make directory
+// syntax --> mkdirSync("path/name", options)
+
+// demo/folder1/folder2/folder3/...........
+// fs.mkdirSync("./demo/fol1/fol2/fol3/app.js", { recursive: true });
+//! recursive : true --> this will recursively call the function until the structure is created, by default it is set to false.
 
 //! create this folder structure -> backend/controller/user.js
 function createStructure() {
@@ -88,4 +92,76 @@ function createStructure() {
   fs.writeFileSync("./backend/controller/user.js", "let a  = 20");
 }
 
-createStructure();
+// createStructure();
+
+// fs.mkdirSync("./backend/controller/user", { recursive: true });
+// fs.writeFileSync("./backend/controller/user.js/user.txt", " ");
+// console.log("created");
+
+//? 6) removing a folder/directory
+// method name --> rmdirSync() --> remove directory
+// syntax --> rmdirSync("path/name", options)
+
+// fs.rmdirSync("./backend", { recursive: true }); // avoid this because it will be deprecated
+
+// fs.unlinkSync("./backend/controller/user.js/user");
+// fs.rmdirSync("./backend/controller/user.js");
+// fs.rmdirSync("./backend/controller");
+// fs.rmdirSync("./backend");
+
+//? 7) to rename a folder or file (using this method we can also move a file or folder to a different path)
+// method name -->  renameSync()
+// syntax -->  renameSync("old path of folder/file", "new path of folder/file")
+
+//! for files
+// fs.renameSync("./demo.txt", "../../app.js");
+// console.log("file renamed");
+//& 1) we are renaming a file
+//& 2) we can also move this file
+
+//! for folders
+// fs.renameSync("../dummy", "./content");
+//& 1) we are renaming a file
+//& 2) we can also move this file
+
+//~ copy the contents of "fs.js" to a new file named "fs.txt"
+// 1) read the contents of fs.js --> let contents = readFileSync()
+// 2) write the contents in new file "fs.txt" --> appendFileSync()
+
+// let contents = fs.readFileSync("./fs.js", "utf-8");
+// fs.appendFileSync("./fs.txt", contents);
+// console.log("file copied");
+
+//? 8) to copy a file
+// method name --< copyFileSync()
+// syntax --> fs.copyFileSync("source", "dest")
+// fs.copyFileSync("./fs.js", "../fs.md");
+
+//~ error first callbacks --> in this, the first parameter is reserved for error in the callback function
+
+//! ===================== asynchronous execution using fs  (using callbacks, then/catch, async-await)===========================
+
+//! 1) reading a file
+//? method name --> readFile()
+//? method name --> readFile("path", "encoding", callback)
+
+// console.log(1);
+// fs.readFile("./index.html", "utf-8", (error, data) => {
+//   if (error) console.log("error occurred", error);
+//   console.log(data);
+// });
+
+// console.log(2);
+// console.log(3);
+
+//! 2) writing a file
+//? method name --> writeFile()
+//? method name --> writeFile("path", "data", callback)
+
+console.log(1);
+fs.writeFile("./demo.txt", "this is txt file", (error, data) => {
+  if (error) console.log(error);
+  console.log("File created");
+});
+console.log(2);
+console.log(3);
