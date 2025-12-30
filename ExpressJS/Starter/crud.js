655e06f182a0ac4ca567dd03258b0a3beaadc981
utkarsh <utkarshgupta.281@gmail.com>
2025-12-30 14:12:07 +0530

    add crud bascis 2.0

--- ExpressJS/Starter/crud.js
+++ ExpressJS/Starter/crud.js
@@ -1,67 +1,94 @@
-let arr = [
-  { id: 1, name: "varun", age: 34 },
-  { id: 2, name: "ashwin", age: 34 },
-  { id: 3, name: "ashwini", age: 32 },
-];
-
 import express from "express";
-import fs from "fs";
+import empDetails from "./db.js";
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true })); //TODO:middleware
+//TODO:middleware
+app.use(express.urlencoded({ extended: true })); // reading the data, which is coming from html form
+app.use(express.json());
 
+//! a simple route to display hello
 app.get("/", (req, res) => {
-  //   throw new Error("Something happened");
+  res.status(200).json({
+    success: true,
+    message: "hello",
+  });
+});
 
-  if (arr.length === 0)
+//! route to display all the data saved in database
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "all users fetched",
+    data: empDetails,
+  });
+});
+
+//~ every data that is written in frontend or postman, is stored in req.body
+//! route to post a user in database
+app.post("/register", (req, res) => {
+  let newUser = req.body;
+  console.log(newUser);
+  empDetails.push(newUser);
+
+  res.status(201).json({
+    success: true,
+    message: "User registration successful",
+    data: newUser,
+  });
+});
+
+//! route to display a single user details (dynamic routing)
+app.get("/one/:id", (req, res) => {
+  let userId = req.params.id;
+
+  console.log(userId);
+
+  let user = empDetails.find((user) => user.id === parseInt(userId));
+
+  if (user === undefined)
     return res.status(404).json({
       success: false,
-      message: "No Users found",
-    }); //? if return is not present, then we will get an error
+      message: "No user found",
+    });
+
+  console.log(user);
 
   res.status(200).json({
     success: true,
-    message: "All users fetched successfully",
-    data: arr,
+    message: "User details fetched successfully",
+    data: user,
   });
 });
 
-//! to display form on the UI
-app.get("/form", (req, res) => {
-  fs.createReadStream("./pages/Register.html", "utf-8").pipe(res);
+app.delete("/delete/:id", (req, res) => {
+  let userId = req.params.id;
+
+  let idx = empDetails.findIndex((user) => user.id === parseInt(userId));
+
+  if (idx !== -1) {
+    empDetails.splice(idx, 1);
+
+    return res.status(200).json({
+      success: true,
+      message: "user deleted",
+      data: empDetails,
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      message: "No user found",
+    });
+  }
 });
 
-//! body property in req object will store the data, which is coming from frontend --> req.body will contain an object
-app.post("/add", (req, res) => {
-  let userData = req.body;
-  arr.push(userData);
-  res.status(201).json({
-    success: true,
-    message: "Registration Successful",
-  });
-});
+app.patch("/update/:id", (req, res) => {});
 
-// app.get("/user/1", (req, res) => {
-//   res.send("user 1");
-// });
-// app.get("/user/2", (req, res) => {
-//   res.send("user 2");
-// }); // this is not possible, to define all the routes
-
-//! dynamic routing
-app.get("/user/:id/:name", (req, res) => {
-  console.log(req.params);
-  const id = parseInt(req.params.id);
-  let user = arr.find((user) => id === user.id);
-  console.log(user);
-  res.send(user);
-});
-//localhost:9000/user/123/abc
-
-http: app.listen(9000, (err) => {
+app.listen(9000, (err) => {
   if (err) console.log(err);
-  console.log("server running");
+  console.log("Server running att port 90000");
 });
 
-//~ it only supports routing (minimal web framework)
+//! steps to work in postman
+//~ 1) create a new workspace
+//~ 2) give a name and click on create
---
+++ ExpressJS/Starter/db.js
@@ -0,0 +1,7 @@
+let empDetails = [
+  { id: 1, name: "varun", age: 34 },
+  { id: 2, name: "ashwini", age: 31 },
+  { id: 4, name: "sri", age: 56 },
+];
+
+export default empDetails;
